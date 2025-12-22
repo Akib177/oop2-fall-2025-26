9271a58cf9a2877ddb06cd3e1fab9c6a9873bc41
Akib177 <adnanakib244@gmail.com>
2025-12-22 14:34:40 +0600

    23-53177-3

---
+++ Lab Final/Deligate/Program.cs
@@ -0,0 +1,31 @@
+﻿using Delegate;
+
+namespace Delegate
+{
+    public delegate int Operation(int a, int b);
+}
+
+class Program
+{
+    static int add(int a, int b)
+    {
+        return a + b;
+    }
+    static int sub(int a, int b)
+    {
+        return a - b;
+    }
+
+    static void Calculate(int a, int b, Operation op)
+    {
+        int result = op(a, b);
+        Console.WriteLine("Result is: " + result);
+    }
+
+    static void Main(string[] args)
+    {
+        Calculate(10, 20, add);
+        Calculate(15, 5, sub);
+    }
+
+}
