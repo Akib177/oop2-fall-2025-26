b112a9cf18b0bb8974f6c06bca3632d198a33454
Akib177 <adnanakib244@gmail.com>
2026-01-20 23:40:24 +0600

    managerial_diet & managerial_package with both backend

--- Project/GreyGym DB/Incidentreport.sql
+++ Project/GreyGym DB/Incidentreport.sql
@@ -5,10 +5,13 @@ CREATE TABLE IncidentReport (
     Description TEXT,
     Status VARCHAR(50)
 );
+
 INSERT INTO IncidentReport (id, ReportedBy, Date, Description, Status) VALUES 
 (1, 'Omi', '2026-01-01', 'dsadasda', 'Resolved')
+
 INSERT INTO IncidentReport (id, ReportedBy, Date, Description, Status) VALUES 
 (2, 'Tanzim', '2026-03-05', 'dsadasda', 'Pending')
+
 INSERT INTO IncidentReport (id, ReportedBy, Date, Description, Status) VALUES 
 (3, 'sady', '2026-03-03', 'dsadasda', 'InProgress');
 
